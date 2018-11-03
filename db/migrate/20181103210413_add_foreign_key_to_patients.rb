3aa8e750f3e9f78fa61eaa83ce3b656852b1dcba
jairorjuela <genius85862@gmail.com>
2018-11-03 18:19:21 -0500

    Add Devise, add translate Devise, add someting style, add table in show for imc

---
+++ db/migrate/20181103210413_add_foreign_key_to_patients.rb
@@ -0,0 +1,5 @@
+class AddForeignKeyToPatients < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :patients, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_03_014621) do
+ActiveRecord::Schema.define(version: 2018_11_03_210413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -156,6 +156,8 @@ ActiveRecord::Schema.define(version: 2018_11_03_014621) do
     t.text "manejo_manual"
     t.text "nivel_de_peso"
     t.text "tiempo_ejecucion_tarea"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_patients_on_user_id"
   end
 
   create_table "psychosocial1s", force: :cascade do |t|
@@ -209,4 +211,17 @@ ActiveRecord::Schema.define(version: 2018_11_03_014621) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "patients", "users"
 end
